cd8349661b86cb1102fb07df6f36d465a396920b
krivopolnik <krivopolnik@gmail.com>
2025-04-08 16:24:52 +0200

    Downgrade Angular to stable v17.2.0, add Material UI and Docker setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -9,6 +10,7 @@ import { errorInterceptor } from './interceptors/error.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
     provideHttpClient(withInterceptors([
       authInterceptor,
       errorInterceptor
