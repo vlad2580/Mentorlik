09e2af0370196ae39e010540d15cc97903aff2bf
Vlad Yurikov <vladyurikov@Vlad-MacBook-Air.local>
2025-04-09 20:20:57 +0200

    Add style to donate page

--- frontend-angular/src/app/pages/donate/donate.component.ts
+++ frontend-angular/src/app/pages/donate/donate.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './donate.component.html',
-  styleUrl: './donate.component.css'
+  styleUrls: ['./donate.component.scss']
 })
 export class DonateComponent {
 
